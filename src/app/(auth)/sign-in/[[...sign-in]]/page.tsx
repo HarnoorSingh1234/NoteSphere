00ae2dcff9d3cbf9200273b15a1d3de00ba5cf3d
HarnoorSingh1234 <harnoorsingharora2005@gmail.com>
2025-05-24 17:19:22 +0530

    ui

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function SignInPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-  <SignIn fallbackRedirectUrl={'/'} />
-  </div>
-  )
-}
+      <SignIn fallbackRedirectUrl={'/'} />
+      </div>
+      )
+    }
