c773991aa92a83e0e87cc9c0e43e83ac9acbab9a
HarnoorSingh1234 <harnoorsingharora2005@gmail.com>
2025-05-29 02:36:35 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import LandingPage from "@/components/landingpage/landingpage";
 import Navbar from "@/components/Navbar";
-import { SignedOut, SignInButton, SignUpButton, SignedIn, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/components/landingpage/landingpage.tsx
+++ src/components/landingpage/landingpage.tsx
@@ -1,5 +1,5 @@
-
 'use client'
+
 import React from 'react'
 import CTASection from './ctasection'
 import FeaturesSection from './featuresection'
@@ -7,10 +7,10 @@ import HeroSection from './herosection'
 import HowItWorksSection from './howitworks'
 import { Footer } from './footer'
 
+
 export default function LandingPage() {
   return (
     <div className="flex flex-col min-h-screen relative overflow-hidden">
-      {/* Canvas background is now moved into the HeroSection */}
       
       <main className="flex-1">
         <HeroSection />
